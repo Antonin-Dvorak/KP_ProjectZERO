3b75717c671487175b2d7206dc002e879ae0d06a
DESKTOP-DLCU5PM\nami <ametsutihoshisora@gmail.com>
2022-03-13 20:28:22 +0900

    ラストシーン、ポストプロセッシング

--- Assets/KP_ProjectZERO/Gimmick/Scripts/GameManager.cs
+++ Assets/KP_ProjectZERO/Gimmick/Scripts/GameManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
 using UnityEngine.SceneManagement;
+using UnityEngine.Rendering.PostProcessing;
 
 public class GameManager : MonoBehaviour
 {
@@ -21,17 +22,25 @@ public class GameManager : MonoBehaviour
 
     [Header("Clear後")]
     [SerializeField] GameObject clearBackImage;
+    [SerializeField] GameObject clearBackImage2;
     [SerializeField] GameObject text5;//よくできたね
     [SerializeField] GameObject sister1;
     [SerializeField] GameObject sister2;
+    [SerializeField] GameObject clearLogo;
+    [SerializeField] GameObject credits;
 
     [Header("Flags")]
     public bool gotBottle = false;
     public bool gotSyringe = false;
     public bool gotWrist = false;
     bool finish = false;
+    bool startGr = false;
 
     [Header("Sounds")]
+    [SerializeField] PostProcessVolume ppVolume;
+    Grain gr;
+    LensDistortion ld;
+
     [SerializeField] AudioSource audioSource;
     [SerializeField] AudioClip clip_kati;//移動ボタン
     [SerializeField] AudioClip clip_tap;//ギミックウィンドウオープン
@@ -41,6 +50,8 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
+        gr = ppVolume.profile.GetSetting<Grain>();
+        ld = ppVolume.profile.GetSetting<LensDistortion>();
         roomNumber = ROOM_FRONT;//開始時は真ん中の部屋を向いている
     }
 
@@ -59,42 +70,92 @@ public class GameManager : MonoBehaviour
         {
             audioSource.Stop();
             SoundSE(clip_appear);
-            clearBackImage.SetActive(true);
-            sister1.SetActive(true);
-            Invoke("Clear0", 3f);
+            clearBackImage.SetActive(true);//手がでる
+            Invoke("Clear1", 3f);
         }
     }
 
-    void Clear0()
-    {
-        text5.SetActive(true);
-        Invoke("Clear1", 3f);
-    }
-    void Clear1()
-    {
-
-        hidePanelObj.SetActive(true);
-        hidePanelObj.GetComponent<Image>().color = new Color(0f, 0f, 0f, 0.0f);
-        hidePanelObj.GetComponent<Image>().DOFade(1f, 12f).SetEase(Ease.Linear).OnComplete(GoStart);
-        Invoke("Clear2", 3f);
-    }
-
-    void Clear2()
+    void Clear1()//フェード
     {
         lastPanelObj.SetActive(true);
         lastPanelObj.GetComponent<Image>().DOFade(1f, 0.5f).SetEase(Ease.InQuad).SetLoops(2, LoopType.Yoyo);
-        Invoke("Clear3", 0.5f);
-    }
-    void Clear3()
-    {
-        sister1.SetActive(false);
-        sister2.SetActive(true);
+        Invoke("Clear2", 0.5f);
     }
 
-    void GoStart()
+    void Clear2()//妹登場、テキスト
+    {
+        clearBackImage2.SetActive(true);
+        sister1.SetActive(true);
+        text5.SetActive(true);
+        Invoke("Clear3", 3f);
+    }
+
+    void Clear3()//フェード
+    {
+        lastPanelObj.SetActive(true);
+        lastPanelObj.GetComponent<Image>().DOFade(1f, 0.5f).SetEase(Ease.InQuad).SetLoops(2, LoopType.Yoyo);
+        Invoke("Clear4", 0.5f);
+    }
+
+    void Clear4()//妹2
+    {
+        sister1.SetActive(true);
+        sister2.SetActive(true);
+        Invoke("Clear5", 2f);
+
+    }
+
+    void Update()
+    {
+
+        if (startGr)
+        {
+                gr.intensity.value += 0.001f;
+                ld.intensity.value -= 0.03f;
+        }
+    }
+    void Clear5()//だんだん暗くなる＋ポスプロ
+    {
+
+        startGr = true;
+        hidePanelObj.SetActive(true);
+        hidePanelObj.GetComponent<Image>().color = new Color(0f, 0f, 0f, 0.0f);
+        hidePanelObj.GetComponent<Image>().DOFade(1f, 4f).SetEase(Ease.Linear).OnComplete(Clear6);
+        Invoke("Clear6", 7f);
+
+    }
+
+    void Clear6()//フェード
+    {
+        startGr = false;
+        text5.SetActive(false);
+        lastPanelObj.SetActive(true);
+        lastPanelObj.GetComponent<Image>().DOFade(1f, 2f).OnComplete(Clear7);
+    }
+
+    void Clear7()//クリアの文字
+    {
+        gr.intensity.value = 0f;
+        ld.intensity.value = 0f;
+
+        sister2.SetActive(false);
+        clearLogo.SetActive(true);
+        Invoke("Clear8", 1.5f);
+    }
+
+    void Clear8()//クレジットと制作
+    {
+        credits.SetActive(true);
+        Invoke("GoStart", 10f);
+    }
+
+    void GoStart()//スタートへ
     {
         SceneManager.LoadScene("Start");
     }
+
+
+
 
     public void PushButtonLeft()
     {
